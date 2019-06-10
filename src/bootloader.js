2339d8058b3f5780322bad577c611dbbd00227ca
Bryan Astacio <brya0196@gmail.com>
2019-06-09 22:27:59 -0400

    created the configuration

---
+++ src/bootloader.js
@@ -0,0 +1,15 @@
+class Bootloader extends Phaser.Scene {
+    constructor() {
+        super({ key: "Bootloader" });
+    }
+
+    preload() {
+        console.log("Se a cargado scena boot loader");
+    }
+
+    create() {
+
+    }
+}
+
+export default Bootloader;
