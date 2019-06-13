ffba3fd8fd97c1b753d906c10a92c954804a4395
Bryan Astacio <brya0196@gmail.com>
2019-06-12 21:06:11 -0400

    added the palas game object and add the game phsics

--- src/scenes/scene_play.js
+++ src/scenes/scene_play.js
@@ -1,3 +1,5 @@
+import Palas from '../gameObjects/palas.js';
+
 class ScenePlay extends Phaser.Scene {
     constructor() {
         super({ key: 'ScenePlay' });
@@ -7,14 +9,27 @@ class ScenePlay extends Phaser.Scene {
         let centerWidth = this.sys.game.config.width / 2;
         let centerHeight = this.sys.game.config.height / 2;
 
+        //separador
         this.add.image( centerWidth, centerHeight, "separador");
 
-        this.izquierda = this.add.image(30, centerHeight, "izquierda");
-        this.derecha = this.add.image(this.sys.game.config.width - 30 , centerHeight, "derecha");
-    
+        //palas
+        this.izquierda = new Palas(this, 30, centerHeight, "izquierda");
+        this.derecha = new Palas(this, this.sys.game.config.width - 30 , centerHeight, "derecha");
+        
+        //bola
+        this.physics.world.setBoundsCollision(false, false, true, true);
         this.ball = this.physics.add.image(centerWidth, centerHeight, "ball");
+        this.ball.setCollideWorldBounds(true);
+        this.ball.setBounce(1);
+        this.ball.setVelocityX(-180);
 
-        this.ball.setVelocity(-180);
+        // Fisicas
+        this.physics.add.collider(this.ball, this.izquierda, this.chocaPala, null, this);
+        this.physics.add.collider(this.ball, this.derecha, this.chocaPala, null, this);
+    }
+
+    chocaPala() {
+        
     }
 }
 
