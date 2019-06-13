33f2acb425ea7715e9255323bd89477e73cb5d37
Bryan Astacio <brya0196@gmail.com>
2019-06-12 21:36:03 -0400

    added movement to the Palas object and the logic of the ball in the pong game

--- src/scenes/scene_play.js
+++ src/scenes/scene_play.js
@@ -6,30 +6,67 @@ class ScenePlay extends Phaser.Scene {
     }
 
     create() {
-        let centerWidth = this.sys.game.config.width / 2;
-        let centerHeight = this.sys.game.config.height / 2;
+        this.centerWidth = this.sys.game.config.width / 2;
+        this.centerHeight = this.sys.game.config.height / 2;
 
         //separador
-        this.add.image( centerWidth, centerHeight, "separador");
+        this.add.image( this.centerWidth, this.centerHeight, "separador");
 
         //palas
-        this.izquierda = new Palas(this, 30, centerHeight, "izquierda");
-        this.derecha = new Palas(this, this.sys.game.config.width - 30 , centerHeight, "derecha");
+        this.left = new Palas(this, 30, this.centerHeight, "izquierda");
+        this.right = new Palas(this, this.sys.game.config.width - 30 , this.centerHeight, "derecha");
         
         //bola
         this.physics.world.setBoundsCollision(false, false, true, true);
-        this.ball = this.physics.add.image(centerWidth, centerHeight, "ball");
+        this.ball = this.physics.add.image( this.centerWidth, this.centerHeight, "ball");
         this.ball.setCollideWorldBounds(true);
         this.ball.setBounce(1);
         this.ball.setVelocityX(-180);
 
         // Fisicas
-        this.physics.add.collider(this.ball, this.izquierda, this.chocaPala, null, this);
-        this.physics.add.collider(this.ball, this.derecha, this.chocaPala, null, this);
+        this.physics.add.collider(this.ball, this.left, this.chocaPala, null, this);
+        this.physics.add.collider(this.ball, this.right, this.chocaPala, null, this);
+
+        // Controles
+        //Pala derecha
+        this.cursor = this.input.keyboard.createCursorKeys();
+        
+        //Pala izquierda
+        this.cursorW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.cursorS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    }
+
+    update() {
+        if (this.ball.x < 0 || this.ball.x > this.sys.game.config.width) {
+            this.ball.setPosition(this.centerWidth, this.centerHeight);
+        }
+
+        // Control de las palas
+        //Pala derecha
+        if(this.cursor.down.isDown) {
+            this.right.body.setVelocityY(300);
+        }
+        else if(this.cursor.up.isDown) {
+            this.right.body.setVelocityY(-300);
+        }
+        else {
+            this.right.body.setVelocityY(0);
+        }
+
+        //Pala izquierda
+        if(this.cursorS.isDown) {
+            this.left.body.setVelocityY(300);
+        }
+        else if(this.cursorW.isDown) {
+            this.left.body.setVelocityY(-300);
+        }
+        else {
+            this.left.body.setVelocityY(0);
+        }
     }
 
     chocaPala() {
-        
+        this.ball.setVelocityY(Phaser.Math.Between(-120, 120));
     }
 }
 
